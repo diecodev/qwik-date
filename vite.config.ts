924e72f4a4c1af8bdf3e6b0a4cae681acf454294
diecodev <diegoddiaz05@gmail.com>
2024-04-29 10:14:35 -0500

    build: 🐛 qwik vite plugin overwrite given lib.entry

--- vite.config.ts
+++ vite.config.ts
@@ -7,24 +7,29 @@ const { dependencies = {}, peerDependencies = {} } = pkg as any;
 const makeRegex = (dep) => new RegExp(`^${dep}(/.*)?$`);
 const excludeAll = (obj) => Object.keys(obj).map(makeRegex);
 
-export default defineConfig(() => {
-  return {
-    build: {
-      target: "es2020",
-      lib: {
-        entry: "./src/lib/index.ts",
-        formats: ["es", "cjs"],
-        fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
+export default defineConfig({
+  plugins: [qwikVite(), tsconfigPaths()],
+  build: {
+    target: "es2020",
+    lib: {
+      entry: {
+        utils: "./src/lib/core/utils/utils.ts",
+        constants: "./src/lib/core/constants/constants.ts",
+        types: "./src/lib/core/types.ts",
       },
-      rollupOptions: {
-        // externalize deps that shouldn't be bundled into the library
-        external: [
-          /^node:.*/,
-          ...excludeAll(dependencies),
-          ...excludeAll(peerDependencies),
-        ],
+      formats: ["es", "cjs"],
+      fileName: (format, entry) => {
+        console.log({ format, entry });
+        return `${entry}.qwik.${format === "es" ? "mjs" : "cjs"}`;
       },
     },
-    plugins: [qwikVite(), tsconfigPaths()],
-  };
+    rollupOptions: {
+      // externalize deps that shouldn't be bundled into the library
+      external: [
+        /^node:.*/,
+        ...excludeAll(dependencies),
+        ...excludeAll(peerDependencies),
+      ],
+    },
+  },
 });
