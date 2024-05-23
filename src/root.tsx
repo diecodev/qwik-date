baf2d6b4cac1ea69263d5bad97f13fd9e891c2df
diecodev <diegoddiaz05@gmail.com>
2024-05-22 23:39:06 -0500

    test(core): 🧪 using the headless version in root (testing purposes)

--- src/root.tsx
+++ src/root.tsx
@@ -1,11 +1,34 @@
-export default () => {
+import { $, component$ } from "@builder.io/qwik";
+import * as Calendar from "./lib/headless";
+
+export default component$(() => {
+  const onDateSelect$ = $((date: string | null) => {
+    return console.log({ date });
+  });
   return (
     <>
       <head>
         <meta charSet="utf-8" />
         <title>Qwik Blank App</title>
       </head>
-      <body></body>
+      <body>
+        <Calendar.Root completeWeeks locale="es" theme="light">
+          <Calendar.Header>
+            <Calendar.HeaderTitle />
+            <Calendar.Previous />
+            <Calendar.Next />
+          </Calendar.Header>
+          <Calendar.Grid>
+            <Calendar.GridWeekdays />
+            <Calendar.GridDays
+              onClick$={(_, target) => {
+                const date = target.getAttribute("data-value");
+                onDateSelect$(date);
+              }}
+            />
+          </Calendar.Grid>
+        </Calendar.Root>
+      </body>
     </>
   );
-};
+});
